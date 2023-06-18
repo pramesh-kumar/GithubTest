b891f7539c107dc8363d8df1f5a94fc79e040978
pramesh-kumar <prameshkumar006@gmail.com>
2023-06-18 18:17:40 +0530

    inital commit

---
+++ sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 20;
+    int b = 40;
+    cout << (a + b) << endl;
+    return 0;
+}
+
+/*
+
+local ----->  stagging area  --------> github
+
+git init
+git status
+
+git add .
+git commit -m "write anything"
+git log
+git add origin  url
+git remote
+git push u origin master
+
+
+*/
